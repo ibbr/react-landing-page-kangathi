4a33d304f93b9a6b6562319ee7034728915862a2
Ibrahim Rasheed <ibrahim00018@gmail.com>
2024-10-10 01:28:07 +0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './logo.jpg';
 import './App.css';
 
 function App() {
@@ -7,16 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Web Development | Software Development | Consultation
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>+960 9559500</p>
       </header>
     </div>
   );
